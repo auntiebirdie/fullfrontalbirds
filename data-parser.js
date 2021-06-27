8f7b5d337cc6691c5d7fe81565a1ffad7d492032
seppukawaii <seppukawaii@gmail.com>
2021-06-27 13:48:59 +0000

    Sync with local copy

---
+++ data-parser.js
@@ -0,0 +1,33 @@
+const csv = require('csv-parser');
+const fs = require('fs');
+
+var output = {};
+
+// Clements, J. F., T. S. Schulenberg, M. J. Iliff, S. M. Billerman, T. A. Fredericks, B. L. Sullivan, and C. L. Wood. 2019.
+// The eBird/Clements Checklist of Birds of the World: v2019.
+// Downloaded from https://www.birds.cornell.edu/clementschecklist/download/ 
+
+fs.createReadStream('eBird_Taxonomy_v2019.csv')
+    .pipe(csv())
+    .on('data', (row) => {
+	if (row['CATEGORY'] == 'species') {
+        var order = row['ORDER1']
+        var family = row['FAMILY'].split(' ').shift();
+
+        if (!output[order]) {
+            output[order] = {}
+        }
+
+        if (!output[order][family]) {
+            output[order][family] = {
+                display: row['FAMILY'],
+                children: []
+            };
+        }
+
+       output[order][family].children.push([row['PRIMARY_COM_NAME'], row['SCI_NAME']]);
+	}
+    })
+    .on('end', () => {
+        fs.writeFileSync('allBirds.json', JSON.stringify(output));
+    });
